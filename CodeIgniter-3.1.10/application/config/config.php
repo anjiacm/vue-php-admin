9ab2518ad3ccdf59d0acdd4445aff13ae923f807
qiaokun <qkbeyond@gmail.com>
2019-12-27 08:53:48 +0800

    composer 安装 php-jwt, config.php里配置 Composer auto-loading及自定义jwt变量配置
    composer require "firebase/php-jwt": "^5.0"

--- CodeIgniter-3.1.10/application/config/config.php
+++ CodeIgniter-3.1.10/application/config/config.php
@@ -136,7 +136,8 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FALSE;
+//$config['composer_autoload'] = FALSE;
+$config['composer_autoload'] = 'vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -521,3 +522,6 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+// pocoyo
+$config['jwt_key'] = 'pocoyo';
