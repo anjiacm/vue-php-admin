57233796c927f3085fa17f3c83e9a64cb27f5a07
qiaokun <qkbeyond@gmail.com>
2020-04-14 11:57:22 +0800

    :new: 增加系统日志查看功能

---
+++ vue-element-admin/src/api/log.js
@@ -0,0 +1,9 @@
+import request from '@/utils/request'
+
+// 查询框或按钮 无参数时查询所有
+export function getLogList(parms) {
+  return request({
+    url: `/sys/log/logs?${parms}`,
+    method: 'get'
+  })
+}
---
+++ vue-element-admin/src/views/sys/log/index.vue
@@ -0,0 +1,222 @@
+<template>
+  <div class="app-container">
+    <div v-perm="['/sys/log/logs/get']" class="filter-container">
+      <el-input v-model="filters[0].value" placeholder="uri" style="width: 200px;" />
+      <el-select v-model="filters[1].value" placeholder="请求方法" clearable>
+        <el-option label="GET" value="get" />
+        <el-option label="POST" value="post" />
+        <el-option label="PUT" value="put" />
+        <el-option label="DELETE" value="delete" />
+      </el-select>
+      <el-date-picker
+        v-model="filters[2].value"
+        :picker-options="pickerOptions"
+        type="datetimerange"
+        format="yyyy-MM-dd HH:mm"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        align="left"
+      />
+    </div>
+
+    <data-tables-server
+      :data="list"
+      :search-def="searchDef"
+      :total="total"
+      :filters="filters"
+      :table-props="tableProps"
+      :loading="listLoading"
+      :page-size="10"
+      :pagination-props="{ background: true, pageSizes: [10,30,50] }"
+      layout="table,pagination"
+      @query-change="fetchData"
+    >
+      <el-table-column
+        v-for="title in titles"
+        :prop="title.prop"
+        :label="title.label"
+        :key="title.label"
+        :sortable="title.sortable"
+      />
+    </data-tables-server>
+  </div>
+</template>
+
+<script>
+import waves from '@/directive/waves' // Waves directive
+import perm from '@/directive/perm/index.js' // 权限判断指令
+import { getLogList } from '@/api/log'
+import _ from 'lodash'
+import timestamp from 'unix-timestamp'
+
+export default {
+  name: 'SysLog',
+  // 所以在编写路由 router 和路由对应的 view component 的时候一定要确保 两者的 name 是完全一致的。
+  directives: { waves, perm },
+  data() {
+    return {
+      searchDef: {
+        show: true,
+        debounce: 3000
+      },
+      defaultQueryInfo: '',
+      filters: [
+        {
+          prop: 'uri',
+          value: ''
+        },
+        {
+          prop: 'method',
+          value: ''
+        },
+        {
+          prop: 'time',
+          value: ''
+        }
+      ],
+      list: [],
+      total: 0,
+      listLoading: true,
+      updateLoading: false,
+      titles: [
+        {
+          prop: 'id',
+          label: 'id',
+          sortable: 'custom' // 指定sortable custom 则为可排序字段，不指定则不排序
+        },
+        {
+          prop: 'username',
+          label: '用户名'
+        },
+        {
+          prop: 'uri',
+          label: 'uri',
+          sortable: 'custom'
+        },
+        {
+          prop: 'method',
+          label: '请求方法',
+          sortable: 'custom'
+        },
+        {
+          prop: 'ip',
+          label: 'IP'
+        },
+        {
+          prop: 'time',
+          label: '创建时间',
+          sortable: 'custom'
+        },
+        {
+          prop: 'rtime',
+          label: '请求耗时（秒)',
+          sortable: 'custom'
+        },
+        {
+          prop: 'response_code',
+          label: '响应code',
+          sortable: 'custom'
+        }
+      ],
+      tableProps: {
+        border: false,
+        stripe: true,
+        highlightCurrentRow: true,
+        defaultSort: {
+          prop: 'id',
+          order: 'ascending'
+        }
+      },
+      pickerOptions: {
+        shortcuts: [
+          {
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+              picker.$emit('pick', [start, end])
+            }
+          },
+          {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+              picker.$emit('pick', [start, end])
+            }
+          },
+          {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+              picker.$emit('pick', [start, end])
+            }
+          }
+        ]
+      }
+    }
+  },
+  created() {},
+  methods: {
+    // 获取数据
+    fetchData(queryInfo) {
+      // console.log('queryInfo', queryInfo)
+      // 页面调用 fetchData 时会清空queryInfo， 重载该状态值
+      // 页面 created 时及翻页时， queryInfo 不会空均会变化， 需要保留此状态，以备fetchData里 queryInfo为空时，重新加载该值
+      !queryInfo
+        ? (queryInfo = this.defaultQueryInfo)
+        : (this.defaultQueryInfo = queryInfo)
+
+      this.listLoading = true
+
+      // GET /users?offset=1&limit=20&fields=id,username,email,listorder&sort=-listorder,+id&query=~username,status&username=admin&status=1
+      // {"type":"init","page":1,"pageSize":5,"sort":{"prop":"listorder","order":"ascending"},"filters":[{"prop":"username","value":""},{"prop":"status","value":""}]}
+      // 初始化分页/排序/查询字符串，与后端一致符合 Restful 风格
+      const offset = queryInfo.page
+      const limit = queryInfo.pageSize
+      const sort =
+        (queryInfo.sort.order === 'ascending' ? '+' : '-') + queryInfo.sort.prop
+
+      // 根据 &query: ~uri,method,time 构造具体参数
+      let queryStr = ''
+      _.forEach(queryInfo.filters, function(value, key) {
+        if (value.value) {
+          if (value.prop !== 'time') {
+            queryStr += '&' + value.prop + '=' + value.value
+          } else if (value.prop === 'time') {
+            // 时间区间为数组 value.value 转成 unixstamp 10000,20000 [1585670400,1586880000]
+            const finalRes = _.map(value.value, function square(item) {
+              return timestamp.fromDate(item)
+            })
+            queryStr += '&' + value.prop + '=' + _.join(finalRes)
+          }
+        }
+      })
+
+      // &query: ~uri,method,time 根据业务情况写死
+      const queryParms =
+        'offset=' +
+        offset +
+        '&limit=' +
+        limit +
+        '&sort=' +
+        sort +
+        '&query=~uri,method,time' +
+        queryStr
+      // console.log(queryParms)
+
+      getLogList(queryParms).then(res => {
+        console.log('getUserList', res)
+        this.list = res.data.items
+        this.total = res.data.total
+        this.listLoading = false
+      })
+    }
+  }
+}
+</script>
